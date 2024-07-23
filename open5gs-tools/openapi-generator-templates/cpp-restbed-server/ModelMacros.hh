6dcfc8e4fd750a0eaaba5dbe45064fa270f71313
David Waring <david.waring2@bbc.co.uk>
2024-07-23 18:03:24 +0100

    OpenAPI C++ templates improvements for nested containers

---
+++ open5gs-tools/openapi-generator-templates/cpp-restbed-server/ModelMacros.hh
@@ -0,0 +1,26 @@
+/**************************************************************************
+ * ModelMacros.hh : ModelMacros model base class/interface
+ **************************************************************************
+ * This is the base type for all model files generated from the OpenAPI
+ * YAML.
+ **************************************************************************
+ * License: 5G-MAG Public License (v1.0)
+ * Author: David Waring
+ * Copyright: (C)2024 British Broadcasting Corporation
+ *
+ * For full license terms please see the LICENSE file distributed with this
+ * program. If this file is missing then the license can be retrieved from
+ * https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
+ */
+
+#ifndef _OPENAPI_MODEL_MACROS_HH_
+#define _OPENAPI_MODEL_MACROS_HH_
+
+#ifndef CONCAT
+#define CONCAT(a,b) a ## b
+#endif
+
+/* vim:ts=8:sts=4:sw=4:expandtab:
+ */
+
+#endif /* _OPENAPI_MODEL_MACROS_HH_ */
